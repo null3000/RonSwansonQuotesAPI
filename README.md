55213d38d9b256cca49b97b5f5009086aec11be7
Null3000 <vaughnbosu@vaughns-mbp.lan>
2022-06-13 21:49:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RonSwansonQuotesAPI
+API to recieve Ron Swanson quotes
+## Endpoint 
+https://RonSwansonQuotesAPI.null3000.repl.co/api/v1/quote
+<p> Returns random Ron Swanson quote
+
+  
+
---
+++ app.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const PORT = process.env.PORT || 4000;
+
+const endpoint = '/api/v1/'
+app.get(endpoint + 'quote', (req, res) => {
+    let quotes = require('./RonSwansonQuotes.json');
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(200).send(quote)
+});
+
+app.listen(
+    PORT,
+    () => console.log(`Server is running on port http://localhost:${PORT}`)
+)
